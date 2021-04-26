5e9f4b03f780d2eff8de961ed25c69f157d2122e
ysulaeman2010 <ysulaeman2010@gmail.com>
2021-04-26 12:42:04 +0700

    create project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { key: Math.random().toString(), name: uName, age: uAge },
+      ];
+    });
+  };
+
+  let content = (
+    <p style={{textAlign: 'center', color:'white'}}>No user, add one</p>
+  )
+
+  if(usersList.length > 0){
+    content = (
+      <UserList users={usersList} />
+    )
+  }
+
+    return (
+    <React.Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <section>
+        {content}
+      </section>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,69 @@
+import React, { useState, useRef } from "react";
+import Wrapper from "../Helpers/Wrapper";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const [error,setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title : 'Invalid Input',
+        message : 'Please enter a valid name and age'
+      })
+      return;
+    }
+
+    if (+enteredUserAge < 1) {
+      setError({
+        title : 'Invalid Age',
+        message : 'Please enter a valid age (> 0)'
+      })
+      return;
+    }
+
+    console.log(enteredName, enteredUserAge);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            ref={nameInputRef}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            type="number"
+            id="age"
+            ref={ageInputRef}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
